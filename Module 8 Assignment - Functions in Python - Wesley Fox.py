f262cc3b551e4b235ae5f346148cd90678271256
Wesley F <scam.email.co@gmail.com>
2026-03-06 21:21:21 -0500

    Add files via upload

---
+++ Module 8 Assignment - Functions in Python - Wesley Fox.py
@@ -0,0 +1,17 @@
+def greater_than(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+# Main program - First test
+a = 2
+b = 3
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+# Second test
+a = 10
+b = 6
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
